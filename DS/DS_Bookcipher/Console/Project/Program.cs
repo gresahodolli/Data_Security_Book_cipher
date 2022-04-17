8b8d43fd31c2e1ded2281bb0327083891749abcc
gresahodolli <94695584+gresahodolli@users.noreply.github.com>
2022-04-17 21:02:37 +0200

    Update Program.cs

--- DS/DS_Bookcipher/Console/Project/Program.cs
+++ DS/DS_Bookcipher/Console/Project/Program.cs
@@ -29,7 +29,30 @@ namespace BookCipher
 
         static readonly string PDFFile = @"C:\Users\OnLine\Desktop\Libri\libri.pdf";
 
-       
+       public static string GetText(string filePath)
+        {
+            var sb = new StringBuilder();
+            try
+            {
+                using (PdfReader reader = new PdfReader(filePath))
+                {
+                    string prevPage = "";
+                    for (int page = 1; page <= reader.NumberOfPages; page++)
+                    {
+                        ITextExtractionStrategy its = new SimpleTextExtractionStrategy();
+                        var s = PdfTextExtractor.GetTextFromPage(reader, page, its);
+                        if (prevPage != s) sb.Append(s);
+                        prevPage = s;
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return sb.ToString();
+        }
 
         
     }
