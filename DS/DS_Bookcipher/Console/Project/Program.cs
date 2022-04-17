3218e79576cdd0f048f8f31a78428f285c794cf7
gresahodolli <94695584+gresahodolli@users.noreply.github.com>
2022-04-17 19:53:03 +0200

    Add files via upload

---
+++ DS/DS_Bookcipher/Console/Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.IO;
+
+namespace BookCipher
+{   
+    class Program
+    {
+        //Fajlli
+        static readonly string textFile = @"C:\Users\OnLine\Desktop\Libri\pjesa1.txt";
+
+        public static string readFile(string textFile)
+        {
+            if(File.Exists(textFile))
+            {
+                string txt = File.ReadAllText(textFile);
+                return txt;
+            }
+            return "Fajlli nuk ekziston\n";
+        }
+
+        static readonly string PDFFile = @"C:\Users\OnLine\Desktop\Libri\libri.pdf";
+
+       
+
+        
+    }
+}
